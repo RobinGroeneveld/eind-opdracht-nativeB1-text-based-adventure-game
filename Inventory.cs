084323adabc5e6af838736ac20fa4337dea5331b
RobinGroeneveld <robin@logicandmore.com>
2025-05-09 18:32:03 +0200

    added story, added logic and classes

---
+++ Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eindopdracht_native_text_based_adventure_game
+{
+    internal class Inventory
+    {
+        public List<string> itemsInInventory = ["Knife", "Bandages"];
+        public void ShowInventory()
+        {
+            Console.WriteLine("Inventory:");
+            foreach (string item in itemsInInventory)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
